c3d92fa2030761f6886194497296918320224663
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2020-12-27 22:59:35 +0900

    project init: make frontend, backend first files

---
+++ front/src/components/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="main">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Main page'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
