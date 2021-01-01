0adde6c1dd24019682ddfa059f9aa4632c83cb7c
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-01 17:42:54 +0900

    Add front page: Add MainPage, GraphPage

---
+++ front/src/pages/GraphPage.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>{{domain}}</div>
+</template>
+
+<script>
+
+
+export default {
+    name: "GraphPage",
+    props: {
+        domain: {
+            type: String
+        }
+    },
+
+</script>
---
+++ front/src/pages/MainPage.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Main Page</div>
+</template>
+
+<script>
+
+export default {
+    name: "MainPage",
+}
+</script>
