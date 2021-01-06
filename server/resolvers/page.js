8c1dddefd8818335c435124fd3d316314a3afd31
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-07 06:13:39 +0900

    Update server graphql query: Add allPagesInfo query

--- server/resolvers/page.js
+++ server/resolvers/page.js
@@ -1,4 +1,46 @@
+import pageModel from '../models/page'
+import domainModel from '../models/domain'
+
+async function getPagesInfo (ids) {
+    let pages = []
+
+    if (ids <= 0) {
+        return pages;
+    }
+
+    for (let index=0; index < ids.length; index++) {
+        await pageModel.findOne({_id: ids[index]}, (err, res) => {
+            if(err){
+                console.log(err);
+            }
+            else{
+                pages.push(res)
+            }
+        });
+    }
+
+    return pages;
+}
+
+
 export default {
-    Query: {},
-    Mutations: {},
+    Query: {
+        /**
+         * @param args \{ domain: ID! \}
+         */
+        allPagesInfo: async (root, args, context) => {
+
+            let domain = await domainModel.findOne({_id: args.domain}, (err, res) => {
+                if(err){
+                    console.log(err);
+                    return null;
+                }
+                else{
+                    return res;
+                }
+            });
+
+            return await getPagesInfo(domain.pages)
+        },
+    },
 }
--- server/schema/index.js
+++ server/schema/index.js
@@ -1,5 +1,16 @@
+import { gql } from 'apollo-server-express'
+
 import domainSchema from './domain'
+import pageSchema from './page'
+
+const linkSchema = gql`
+    type Query {
+        _empty: String
+    }
+`;
 
 export default [
+    linkSchema,
     domainSchema,
+    pageSchema
 ];
