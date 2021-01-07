998337dd8c2ffe1a65433096cabb8ac4410246fc
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-07 12:49:03 +0900

    Update front component: Query allPagesInfo and add PageNode when user click DomainNode

--- front/src/components/FlowGraph.vue
+++ front/src/components/FlowGraph.vue
@@ -4,6 +4,7 @@
             :elements="elements"
             :nodeTypes="nodeTypes"
             :style="graphStyles"
+            :onElementClick="onElementClick"
         />
     </div>
 </template>
@@ -13,8 +14,9 @@
 import ReactFlow from 'react-flow-renderer';
 
 import { DOMAININFO_QUERY } from '@/graphql/domain'
+import { ALLPAGESINFO_QUERY } from '@/graphql/page'
 
-import { DomainNode } from './react-flow-custom/FlowGraphNode'
+import { DomainNode, PageNode } from './react-flow-custom/FlowGraphNode'
 
 export default {
     name: "FlowGraph",
@@ -26,7 +28,9 @@ export default {
     components: { ReactFlow },
     computed: {
         elements () {
-            let nodes = this.domainNodes;
+            let nodes = this.domainNodes.concat(this.pageNodes);
+            nodes = nodes.concat(this.edgeNodes);
+
             return nodes;
         },
     },
@@ -39,7 +43,70 @@ export default {
             graphStyles: { width: "100%", height: "500px" },
 
             domainNodes: [],
+            pageNodes: [],
+            edgeNodes: [],
 
+            isPageOpened: false
+        }
+    },
+    methods: {
+        onElementClick(event, element){
+            if (element.type === "domain"){
+                this.onDomainClick(event, element);
+            }
+        },
+        onDomainClick(event, element){
+
+            // If already opened, close nodes
+            if (this.isPageOpened) {
+                this.pageNodes = [];
+                this.edgeNodes = [];
+                this.isPageOpened = false;
+                return;
+            }
+
+            // Add page node
+            this.$apollo.query({
+                query: ALLPAGESINFO_QUERY,
+                variables : {
+                    domain: element.data._id
+                },
+            })
+            .then(res => {
+                let pages = [];
+                let edges = [];
+                
+                let x = element.position.x + 200;
+                let y = element.position.y;
+
+                res.data.allPagesInfo.forEach((page, index) => {
+
+                    // Add node
+                    pages.push({
+                        id: page._id,
+                        type: 'page',
+                        data: { ...page },
+                        position: { x: x, y: y },
+                    });
+
+                    y += 100;
+
+                    // Add edge
+                    edges.push({
+                        id: "e" + element.data._id + "-" + page._id,
+                        source: element.data._id,
+                        target: page._id
+                    });
+                });
+
+                this.pageNodes = this.pageNodes.concat(pages);
+                this.$nextTick(function () {
+                    this.edgeNodes = this.edgeNodes.concat(edges);
+                })
+
+                this.isPageOpened = true;
+            })
+            .catch(err => {console.log(err);})
         }
     },
     apollo: {
