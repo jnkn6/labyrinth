59b1d2bcbe812033689b5b3e3d6b0e37d351d80d
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-11 16:49:29 +0900

    Add front vuex getters: Add graphElements getter

--- front/src/store/getters.js
+++ front/src/store/getters.js
@@ -1,3 +1,7 @@
 export default{
-
+    graphElements(state){
+        let graphElements = state.domainNodes.concat(state.pageNodes);
+        graphElements = graphElements.concat(state.edges);
+        return graphElements;
+    },
 }
