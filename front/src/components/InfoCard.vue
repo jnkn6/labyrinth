a7d5d682e88e7916885904daa4da286f564eea43
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-12 19:30:33 +0900

    Add front component: Add InfoCard component

---
+++ front/src/components/InfoCard.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-card elevation="10">
+        <v-card-actions>
+            <v-spacer />
+            <v-btn x-small dark color="red" @click="onClose">
+                <v-icon small dark>mdi-close</v-icon>
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+<script>
+
+
+export default {
+    name: "InfoCard",
+    props: {
+        mode: {
+            type: String
+        },
+        node: {
+            type: Object
+        }
+    },
+    methods: {
+        onClose(){
+            this.$emit('onClose')
+        },
+    }
+}
+</script>
