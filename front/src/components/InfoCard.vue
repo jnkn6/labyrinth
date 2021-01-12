7601916af29be003474c06fbce90bb1af25493bf
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-12 19:36:00 +0900

    Update front component: Use DomainInfoCard component at InfoCard component

--- front/src/components/InfoCard.vue
+++ front/src/components/InfoCard.vue
@@ -6,19 +6,34 @@
                 <v-icon small dark>mdi-close</v-icon>
             </v-btn>
         </v-card-actions>
+        <domain-info-card
+            v-if="node.type === 'domain'"
+            :mode="infoMode"
+            :node="node"
+        />
     </v-card>
 </template>
 <script>
 
+import { modes } from '@/utils/const'
 
+import DomainInfoCard from './DomainInfoCard'
 export default {
     name: "InfoCard",
+    components: {
+        DomainInfoCard,
+    },
     props: {
         mode: {
             type: String
         },
         node: {
             type: Object
+        }
+    },
+    data(){
+        return {
+            infoMode: this.mode,
         }
     },
     methods: {
