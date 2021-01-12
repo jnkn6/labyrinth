d683565b145aa99915204a8cb7376dc27a6bce7c
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-12 19:32:50 +0900

    Add front component: Add domainInfoCard component

---
+++ front/src/components/DomainInfoCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-card-title>{{title}}</v-card-title>
+</template>
+<script>
+
+import { modes } from '@/utils/const'
+
+export default {
+    name: "DomainInfoCard",
+    props: {
+        mode: {
+            type: String
+        },
+        node: {
+            type: Object
+        }
+    },
+    computed: {
+        title() {
+            switch(this.mode){
+                case modes.READ_DOMAIN_INFO:
+                    return 'Domain infomation';
+            }
+        },
+    }
+}
+</script>
