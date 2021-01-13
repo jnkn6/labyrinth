768a6f2ed5901a38ce1e313b36faf5714ca75353
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-14 01:34:59 +0900

    Update front component:  Modify InfoCard component to suit InfoCardSlot component

--- front/src/components/InfoCard.vue
+++ front/src/components/InfoCard.vue
@@ -1,37 +1,18 @@
 <template>
-    <v-card elevation="10">
-        <v-card-actions>
-            <v-spacer />
-            <v-btn x-small dark color="red" @click="onClose">
-                <v-icon small dark>mdi-close</v-icon>
-            </v-btn>
-        </v-card-actions>
+    <div>
         <domain-info-card
             v-if="node.type === 'domain'"
-            :mode="infoMode"
+            :mode="mode"
             :node="node"
+            @onClose="$emit('onClose')"
         />
         <page-info-card 
             v-if="node.type === 'page'"
-            :mode="infoMode"
+            :mode="mode"
             :node="node"
+            @onClose="$emit('onClose')"
         />
-        <v-card-actions>
-            <v-spacer />
-            <v-btn v-if="!isEditing" dark @click="onEdit">
-                <v-icon dark>
-                    mdi-pencil
-                </v-icon>
-                Edit
-            </v-btn>
-            <v-btn v-if="isEditing" dark color="green" @click="onSave">
-                <v-icon dark>
-                    mdi-checkbox-marked-circle
-                </v-icon>
-                Save
-            </v-btn>
-        </v-card-actions>
-    </v-card>
+    </div>
 </template>
 <script>
 
@@ -54,34 +35,5 @@ export default {
             type: Object
         }
     },
-    data(){
-        return {
-            isEditing: modesCategory.EDIT.includes(this.mode),
-            infoMode: this.mode,
-        }
-    },
-    methods: {
-        onClose(){
-            this.$emit('onClose')
-        },
-        onEdit(){
-            this.isEditing = true;
-            if (modesCategory.DOMAIN.includes(this.mode)){
-                this.infoMode = modes.EDIT_DOMAIN;
-            }
-            else if (modesCategory.PAGE.includes(this.mode)){
-                this.infoMode = modes.EDIT_PAGE;
-            }
-        },
-        onSave(){
-            this.isEditing = false;
-            if (modesCategory.DOMAIN.includes(this.mode)){
-                this.infoMode = modes.READ_DOMAIN_INFO;
-            }
-            else if (modesCategory.PAGE.includes(this.mode)){
-                this.infoMode = modes.READ_PAGE_INFO;
-            }
-        }
-    }
 }
 </script>
