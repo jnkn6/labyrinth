6526b4d748a02c6757d8c54250904e9d740ae03b
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-13 23:47:26 +0900

    Add front component: Add InfoCardSlot component

---
+++ front/src/components/InfoCardSlot.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-card elevation="10">
+        <v-card-actions>
+            <v-spacer />
+            <v-btn x-small dark color="red" @click="$emit('onClose')">
+                <v-icon small dark>mdi-close</v-icon>
+            </v-btn>
+        </v-card-actions>
+        <v-card-title>
+            <slot name="title"></slot>
+        </v-card-title>
+
+        <v-card-text>
+            <slot name="readInfo" v-if="!isEditing"></slot>
+            <slot name="editInfo" v-else></slot>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer />
+            <v-btn v-if="!isEditing" dark @click="$emit('onEdit')">
+                <v-icon dark>
+                    mdi-pencil
+                </v-icon>
+                Edit
+            </v-btn>
+            <v-btn v-else dark color="green" @click="$emit('onSave')">
+                <v-icon dark>
+                    mdi-checkbox-marked-circle
+                </v-icon>
+                Save
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+<script>
+
+export default {
+    name: "InfoCardSlot",
+    props: {
+        isEditing: {
+            type: Boolean
+        },
+    },
+}
+</script>
