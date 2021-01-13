b64604ab131a58f562f79ae840782bfb367bcc12
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-14 01:39:01 +0900

    Update front component: Use InfoCardSlot component at PageInfoCard component

--- front/src/components/PageInfoCard.vue
+++ front/src/components/PageInfoCard.vue
@@ -1,20 +1,44 @@
 <template>
-    <v-container>
-        <v-card-title>{{title}}</v-card-title>
-        <v-card-text>
-            <v-row justify="space-around">
-                <v-col>
-                    <div>name: {{node.data.name}}</div>
-                    <div>path: {{node.data.path}}</div>
-                    <div>memo: {{node.data.memo}}</div>
-                </v-col>
-            </v-row>
-        </v-card-text>
-    </v-container>
+    <info-card-slot
+        @onClose="onClose"
+        @onSave="onSave"
+        @onEdit="onEdit"
+        :isEditing="isEditing"
+    >
+        <div slot="title">{{title}}</div>
+        <div slot="readInfo">
+            <p>name: {{name}}</p>
+            <p>path: {{path}}</p>
+            <p>memo: {{memo}}</p>
+        </div>
+        <div slot="editInfo">
+            <v-text-field
+                v-model="name"
+                label="Page name"
+            />
+            <v-text-field
+                v-model="path"
+                label="Page path"
+            />
+            <v-textarea
+                v-model="memo"
+                label="Memo"
+                auto-grow
+                outlined
+            >
+                <template v-slot:label>
+                    <div>
+                        Memo<small>(optional)</small>
+                    </div>
+                </template>
+            </v-textarea>
+        </div>
+    </info-card-slot>
 </template>
 <script>
 
-import { modes } from '@/utils/const'
+import { modes, modesCategory } from '@/utils/const'
+import InfoCardSlot from './InfoCardSlot'
 
 export default {
     name: "PageInfoCard",
@@ -24,11 +48,38 @@ export default {
         },
         node: {
             type: Object
+        },
+    },
+    watch: {
+        mode:{
+            handler(next, prev){
+                this.infoMode = next;
+            }
+        },
+        node: {
+            handler(next, prev){
+                this.name = next.data.name;
+                this.path = next.data.path;
+                this.memo = next.data.memo;
+
+            }
+        },
+    },
+    components: {
+        InfoCardSlot
+    },
+    data(){
+        return {
+            name: this.node.data.name,
+            path: this.node.data.path,
+            memo: this.node.data.memo,
+
+            infoMode: this.mode,
         }
     },
     computed: {
         title() {
-            switch(this.mode){
+            switch(this.infoMode){
                 case modes.CREATE_NEW_PAGE:
                     return 'Create new page';
                 case modes.EDIT_PAGE:
@@ -37,7 +88,21 @@ export default {
                     return 'Page informtaion';
             }
         },
+        isEditing() {
+            return modesCategory.EDIT.includes(this.infoMode)
+        },
+    },
+    methods: {
+        onClose(){
+            this.$emit('onClose')
+        },
+        onEdit(){
+            this.infoMode = modes.EDIT_PAGE;
+        },
+        onSave(){
+            this.infoMode = modes.READ_PAGE_INFO;
 
-    }
+        },
+    },
 }
 </script>
