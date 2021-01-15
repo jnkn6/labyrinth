573a9a603310cae8780ca4dfc89b6c445cdb7c21
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-16 00:04:51 +0900

    front: Add component - Add MemoEditor

---
+++ front/src/components/MemoEditor.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="easymde">
+        <textarea/>
+    </div>
+</template>
+<script>
+
+import EasyMDE from 'easymde'
+import 'easymde/dist/easymde.min.css'
+
+
+export default {
+    name: 'MemoEditor',
+    props:{
+        value: String,
+    },
+    data(){
+        return {
+            config: {},
+            easymde: null,
+        }
+    },
+    mounted(){
+        this.config = {
+            element: this.$el.firstElementChild,
+            renderingConfig: {
+                singleLineBreaks: false,
+                codeSyntaxHighlighting: true,
+            },
+            initialValue: this.value,
+        }
+        this.easymde = new EasyMDE(this.config);
+
+    },
+}
+</script>
