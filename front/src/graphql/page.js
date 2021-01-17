2e854504a501904c9d8db145274175e0ee96083d
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-17 17:06:41 +0900

    front: Add graphql modifyPage mutation

--- front/src/graphql/page.js
+++ front/src/graphql/page.js
@@ -13,3 +13,17 @@ export const ALLPAGESINFO_QUERY = gql`
         }
     }
 `
+
+export const MODIFYPAGE_MUTATION = gql`
+    mutation modifyPage ($page: PageInput!) {
+        modifyPage (page: $page) {
+            _id
+            name
+            path
+            domain
+            groups
+            components
+            memo
+        }
+    }
+`
