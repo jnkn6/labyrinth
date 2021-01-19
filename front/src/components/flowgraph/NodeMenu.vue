5732fb5c1fe443be3a1d0fbbb9b8b5d6b4359b3e
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-20 00:03:40 +0900

    front: Add component - Add NodeMenu component

---
+++ front/src/components/flowgraph/NodeMenu.vue
@@ -0,0 +1,68 @@
+<template>
+<div>
+    <v-list v-if="selectedMenuNode.type === 'domain'">
+        <v-subheader>Domain Menu</v-subheader>
+        <v-list-item @click="onClickOpenPages">
+            <v-list-item-avatar>
+                <v-icon >mdi-file</v-icon>
+            </v-list-item-avatar>
+            <v-list-item-content v-if="!isPageOpened">
+                Open pages
+            </v-list-item-content>
+            <v-list-item-content v-else>
+                Close pages
+            </v-list-item-content>
+        </v-list-item>
+    </v-list>
+    <v-list v-else-if="selectedMenuNode.type === 'page'">
+        <v-subheader>Page Menu</v-subheader>
+            <v-list-item>
+                <v-list-item-title>
+                Open components
+                </v-list-item-title>
+            </v-list-item>
+    </v-list>
+</div>
+</template>
+<script>
+
+import { mapActions } from 'vuex'
+
+export default {
+    name: "NodeMenu",
+    props: {
+        selectedMenuNode : {
+            type: Object
+        }
+    },
+    data(){
+        return {
+            isPageOpened: false,
+        }
+    },
+    methods: {
+        ...mapActions([
+            'fetchPageNodes',
+            'emptyPageNodes',
+            'emptyEdges',
+        ]),
+        onClickOpenPages(){
+            // If already opened, close nodes
+            if (this.isPageOpened) {
+                this.emptyPageNodes();
+                this.emptyEdges();
+                this.isPageOpened = false;
+                return;
+            }
+
+            // Add page/edge
+            this.fetchPageNodes({
+                vue: this,
+                domainNode: this.selectedMenuNode
+            }).then(() => {
+                this.isPageOpened = true;
+            })
+        }
+    }
+}
+</script>
