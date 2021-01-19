369d6145a3f2cc4fc1509592c95d4a33e1379a58
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-19 18:46:42 +0900

    front: Update component - Use AppHeader at GraphPage page

--- front/src/pages/GraphPage.vue
+++ front/src/pages/GraphPage.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <app-header />
         <sidebar :size="sidebarSize" />
         <flow-graph :domain="domain"/>
     </div>
@@ -7,7 +8,7 @@
 
 <script>
 
-import Sidebar from '@/components/Sidebar'
+import AppHeader from '@/components/AppHeader'
 import FlowGraph from '@/components/flowgraph/FlowGraph'
 
 import { mapState } from 'vuex'
@@ -21,6 +22,7 @@ export default {
     },
     components: {
         Sidebar,
+        AppHeader,
         FlowGraph
     },
     computed: {
