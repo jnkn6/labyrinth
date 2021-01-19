1f69a773f73d18cd8f66331cc1a9ba9bbf8d1a42
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-19 18:51:57 +0900

    front: Update component - Change style of InfoCardSlot

--- front/src/components/InfoCardSlot.vue
+++ front/src/components/InfoCardSlot.vue
@@ -2,10 +2,11 @@
     <v-card elevation="10">
         <v-card-actions>
             <v-spacer />
-            <v-btn x-small dark color="red" @click="$emit('onClose')">
-                <v-icon small dark>mdi-close</v-icon>
+            <v-btn icon outlined @click="$emit('onClose')">
+                <v-icon >mdi-close</v-icon>
             </v-btn>
         </v-card-actions>
+
         <v-card-title>
             <slot name="title"></slot>
         </v-card-title>
@@ -17,21 +18,21 @@
 
         <v-card-actions>
             <v-spacer />
-            <v-btn v-if="!isEditing" dark @click="$emit('onEdit')">
-                <v-icon dark>
+            <v-btn elevation="3" v-if="!isEditing" rounded @click="$emit('onEdit')">
+                <v-icon>
                     mdi-pencil
                 </v-icon>
                 Edit
             </v-btn>
             <div v-else>
-                <v-btn dark color="red" @click="$emit('onCancel')">
-                    <v-icon dark>
+                <v-btn elevation="1" rounded @click="$emit('onCancel')">
+                    <v-icon>
                         mdi-cancel
                     </v-icon>
                     Cancel
                 </v-btn>
-                <v-btn dark color="green" @click="$emit('onSave')">
-                    <v-icon dark>
+                <v-btn elevation="3" color="#99ff00" rounded @click="$emit('onSave')">
+                    <v-icon>
                         mdi-checkbox-marked-circle
                     </v-icon>
                     Save
