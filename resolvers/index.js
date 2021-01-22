924efa4cff025f0b2e2a8c353d5b735052b01f98
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-01-22 23:07:50 +0900

    server: Add allImages graphql query

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,7 +1,9 @@
 import domainResolver from './domain'
 import pageResolver from './page'
+import imageResolver from './image'
 
 export default [
     domainResolver,
     pageResolver,
+    imageResolver,
 ];
