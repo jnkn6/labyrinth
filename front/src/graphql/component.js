8535115e1185d8f9d4a21398aa1247025bfb13e2
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-02-04 18:31:57 +0900

    front: Update graphql - Add allComponentsInfo query

--- front/src/graphql/component.js
+++ front/src/graphql/component.js
@@ -1,5 +1,20 @@
 import gql from 'graphql-tag'
 
+export const ALLCOMPONENTSINFO_QUERY = gql`
+    query allComponentsInfo ($page: ID!) {
+        allComponentsInfo(page: $page) {
+            _id
+            name
+            domain
+            page
+            parent
+            groups
+            components
+            memo
+        }
+    }
+`
+
 export const CREATECOMPONENT_MUTATION = gql`
     mutation createComponent ($component: ComponentInput!) {
         createComponent (component: $component) {
