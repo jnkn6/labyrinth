2554657ed1d3132ed19489f5e43f64945a2064f3
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-02-04 23:30:47 +0900

    front: Add Component - DomainAddButton

---
+++ front/src/components/DomainAddButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <span>
+            <p>{{domain}} is NOT registered.</p> 
+            <p>Click the button if you want to register it.</p>
+        </span>
+        <v-btn dark color="#ff2a6d">Add Domain {{domain}}</v-btn>
+    </div>
+</template>
+<script>
+export default {
+    name: "DomainAddButton",
+    props: {
+        domain: {
+            type: String
+        }
+    },
+}
+</script>
+<style scoped>
+.v-btn {
+    text-transform:none;
+}
+</style>
