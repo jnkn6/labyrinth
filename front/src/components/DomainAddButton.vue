af59e7cd60ba0c3e25407eb3688ad6eca4ae739a
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-02-05 00:04:28 +0900

    front: Update component - Add onAddDomain handler at DomainAddButton

--- front/src/components/DomainAddButton.vue
+++ front/src/components/DomainAddButton.vue
@@ -4,16 +4,30 @@
             <p>{{domain}} is NOT registered.</p> 
             <p>Click the button if you want to register it.</p>
         </span>
-        <v-btn dark color="#ff2a6d">Add Domain {{domain}}</v-btn>
+        <v-btn dark color="#ff2a6d" @click="onAddDomain">Add Domain {{domain}}</v-btn>
     </div>
 </template>
 <script>
+
+import { mapActions } from 'vuex'
+
 export default {
     name: "DomainAddButton",
     props: {
         domain: {
             type: String
         }
+    },
+    methods: {
+        ...mapActions([
+            'createDomainNode'
+        ]),
+        onAddDomain(){
+            this.createDomainNode({
+                vue: this,
+                url: this.domain,
+            });
+        },
     },
 }
 </script>
