b9ca75164394be2daeb1b3cba307cc9833826a72
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-02-12 04:43:29 +0900

    front: Update graphql - Add done at checklist query

--- front/src/graphql/checklist.js
+++ front/src/graphql/checklist.js
@@ -4,6 +4,7 @@ export const CHECKLIST_QUERY = gql`
     query checklist ($id: ID!) {
         checklist(id: $id) {
             _id
+            done
             timestamp
             deactivated
         }
