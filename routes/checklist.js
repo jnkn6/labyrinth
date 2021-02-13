71d3208fd78da9434b024ca7ca937cb8450db9c2
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-02-14 03:39:39 +0900

    server: Refactoring resource - Rename resources

--- routes/checklist.js
+++ routes/checklist.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
-import checklist from '../checklist/checklist'
+import checklist from '../resources/checklist/checklist'
 
 const parser = bodyParser.json()
 
