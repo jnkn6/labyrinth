1127d834280e2e0cd761263e1e33afcbcf727299
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-02-15 02:40:32 +0900

    server: Update resource handler - Change init routine

--- resources/nodetype/nodetype.js
+++ resources/nodetype/nodetype.js
@@ -1,73 +1,82 @@
-const type = require('./data/nodetype.json');
+const comptype = require('./data/comptype.json');
 
 class NodeType{
-    constructor(nodetype) {
-        this.originNodetype = nodetype;
-
+    constructor() {
+        this.originNodetype = {};
         this.nodetype = {};
-        this.originNodetype.forEach(element => {
-            this._parse(element, "");
-        });
-
         this.nodetypeOrder = {};
-        this.originNodetype.forEach(root => {
-            this.nodetypeOrder[root.code] = this._extractOrder(root.code);
+    }
+
+    setNodetype(key, nodetype) {
+        this.originNodetype[key] = nodetype;
+
+        this.nodetype[key] = {};
+        this.originNodetype[key].forEach(element => {
+            this._parse(key, element, "");
         });
 
+        this.nodetypeOrder[key] = {};
+        this.originNodetype[key].forEach(root => {
+            this.nodetypeOrder[key][root.code] = this._extractOrder(key, root.code);
+        });
     }
 
     /**
-     * nodetype: {
+     * {
+     *  component: {
      *      form: { vul: [input] },
      *      form_checkbox: { vul: [input] },
      *      form_file: { vul: ["busl_upload-filetype"] },
      *      button: {},
      *      'user-text': { vul: ["input_xss"] }
+     *  }
      * }
      */
-    _parse(element, parentsCode) {
+    _parse(key, element, parentsCode) {
         let code = parentsCode;
         if (parentsCode !== ""){
             code += "_";
         }
         code += element.code;
 
-        this.nodetype[code] = {
+        this.nodetype[key][code] = {
             name: element.name
         };
 
         if (element.vul){ // Override parent's vul
-            this.nodetype[code].vul = element.vul;
+            this.nodetype[key][code].vul = element.vul;
         }
         else{ // Inherit parent's vul
             if(parentsCode !== ""){
-                this.nodetype[code].vul = this.nodetype[parentsCode].vul;
+                this.nodetype[key][code].vul = this.nodetype[key][parentsCode].vul;
             }
         }
 
         if(element.children){
             // recursive
             element.children.forEach(child => {
-                this._parse(child, code);
+                this._parse(key, child, code);
             })
         }
     }
 
     /**  // only one level order
      * {
-     *  "form": [
-     *      "form",
-     *      "form_checkbox",
-     *      "form_select",
-     *      ...
-     *  ],
-     *  "button": [
-     *      "button"
-     *  ],
+     *  component: {
+     *      "form": [
+     *          "form",
+     *          "form_checkbox",
+     *          "form_select",
+     *          ...
+     *      ],
+     *      "button": [
+     *          "button"
+     *      ],
+     *  }
      * }
      */
-    _extractOrder(rootCode){
-        let keys = Object.keys(this.nodetype);
+    _extractOrder(key, rootCode){
+        let keys = Object.keys(this.nodetype[key]);
         let children = [];
 
         keys.forEach(code => {
@@ -80,4 +89,7 @@ class NodeType{
     }
 }
 
-export default new NodeType(type);
+let nodetype = new NodeType();
+nodetype.setNodetype("component", comptype);
+
+export default nodetype;
