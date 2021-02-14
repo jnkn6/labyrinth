d673b8d831d6c6bc668deae81d09e56792ca35e9
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-02-15 06:16:09 +0900

    server: Add routes - Add nodetype route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import filepond from './filepond'
 import checklist from './checklist'
+import nodetype from './nodetype'
 
 const router = express.Router();
 router.use('/upload', filepond);
 router.use('/checklist', checklist);
+router.use('/nodetype', nodetype);
 
 export default router
