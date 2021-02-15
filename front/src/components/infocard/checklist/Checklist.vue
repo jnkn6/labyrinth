924edae3d736ca33dd73f91fd6b855092d2f9409
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-02-16 07:53:30 +0900

    front: Refactoring component - Move Checklist tree code to new component ChecklistTree

--- front/src/components/infocard/checklist/Checklist.vue
+++ front/src/components/infocard/checklist/Checklist.vue
@@ -39,46 +39,14 @@
             </v-menu>
         </v-toolbar>
 
+        <!-- Checklist treeview and filter -->
         <v-card-text>
             <v-container>
                 <v-row>
                     <v-col>
                         <nodetype :node="node" @changeNodetype="onChangeNodetype"/>
 
-                        <!-- Checklist treeview -->
-                        <v-treeview
-                            :key="Math.random()"
-                            open-all
-                            :items="checklist"
-                            item-key="code"
-                            selectable
-                            selected-color="#ff2a6d"
-                            selection-type="leaf"
-                            item-disabled="deactivated"
-                            dense
-                            shaped
-                            hoverable
-                            v-model="done"
-                            return-object
-                        >
-                            <template #append="{item}">
-                                <v-btn
-                                    v-if="deactivated.includes(item.code)"
-                                    icon
-                                    @click="onClickActivate(item)"
-                                >
-                                    <v-icon>mdi-eye-check-outline</v-icon>
-                                </v-btn>
-                                <v-btn
-                                    v-else
-                                    icon
-                                    @click="onClickDeactivate(item)"
-                                >
-                                    <v-icon>mdi-eye-off-outline </v-icon>
-                                </v-btn>
-                            </template>
-                        </v-treeview>
-
+                        <checklist-tree v-if="checklist.length !== 0" :node="node" :original="checklist"/>
                     </v-col>
                 </v-row>
             </v-container>
@@ -88,24 +56,14 @@
 <script>
 
 import api from '@/api'
-import _ from 'lodash'
-
-import moment from 'moment'
-import 'moment-timezone'
-
-import {
-    CHECKLIST_QUERY,
-    CHECK_MUTATION,
-    UNCHECK_MUTATION,
-    ACTIVATE_MUTATION,
-    DEACTIVATE_MUTATION,
-} from '@/graphql/checklist'
 
 import Nodetype from '@/components/infocard/Nodetype'
+import ChecklistTree from './ChecklistTree'
 
 export default {
     components: {
         Nodetype,
+        ChecklistTree,
     },
     name: "Checklist",
     props: {
@@ -116,33 +74,13 @@ export default {
     watch: {
         node: function(){
             this.getChecklistFormat();
-            this.doneFirstWatch = false;
         },
         selectedMenu: function(){
             this.getChecklistFormat();
-            this.doneFirstWatch = false;
         },
         expand: function(){
             this.getChecklistFormat();
-            this.doneFirstWatch = false;
         },
-        done: function(next, prev){
-            if(!this.doneFirstWatch){
-                this.doneFirstWatch = true;
-                return;
-            }
-            if (next.length > prev.length){
-                // If Check
-                // Get new checked list
-                let newCheck = _.differenceBy(next, prev, 'code');
-                this.onClickCheck(newCheck);
-            }
-            else{
-                // If Uncheck
-                let unCheck = _.differenceBy(prev, next, 'code');
-                this.onClickUncheck(unCheck);
-            }
-        }
     },
     data(){
         return {
@@ -151,11 +89,6 @@ export default {
             expand: 1,
 
             checklist: [],
-            done: [],
-            timestamp: [],
-
-            doneFirstWatch: false,
-            deactivated: [],
         }
     },
     methods: {
@@ -168,7 +101,6 @@ export default {
             api.post(`/checklist/${this.node.type}`, payload)
                 .then(res => {
                     this.checklist = res.data;
-                    this.fetchChecklist()
                 });
         },
         getChecklistMenu(){
@@ -176,142 +108,6 @@ export default {
                 .then(res => {
                     this.checkMenu = res.data;
                 });
-        },
-        fetchChecklist(){
-
-            // get done/deactivated list
-            this.$apollo.query({
-                query: CHECKLIST_QUERY,
-                variables : {
-                    id: this.node.data.checklist,
-                },
-            }).then(res => {
-                this.done = JSON.parse(res.data.checklist.done);
-                this.timestamp = JSON.parse(res.data.checklist.timestamp);
-
-                this.timestamp.forEach(element => {
-                    this.addDoneDate(this.checklist, element.code, element.date);
-                });
-
-                this.deactivated = res.data.checklist.deactivated;
-                this.deactivated.forEach(code => {
-                    this.addDeactivated(this.checklist, code);
-                });
-            });
-        },
-        addDeactivated(parent, code){
-            let child = _.filter(parent, {children: [{code: code}]});
-
-            if (child.length !== 0){
-                child.forEach(element => {
-                    if (element.children){
-                        this.addDeactivated(element.children, code)
-                    }
-                });
-            }
-            else{
-                let child = _.filter(parent, {code: code});
-                child.forEach(element => {
-                    element.deactivated = true;
-                });
-            }
-        },
-        addDoneDate(parent, code, date){
-            let child = _.filter(parent, {children: [{code: code}]});
-
-            if (child.length !== 0){
-                child.forEach(element => {
-                    if (element.children){
-                        this.addDoneDate(element.children, code, date)
-                    }
-                });
-            }
-            else{
-                let child = _.filter(parent, {code: code});
-                child.forEach(element => {
-                    this.setElementDate(element, date)
-                });
-            }
-        },
-        setElementDate(element, date){
-            // If first done
-            if(element.date === undefined){
-                element.originalName = element.name;
-            }
-
-            element.date = moment(date);
-
-            const dateStr = element.date.format('YYYY-MM-DD HH:mm:ss');
-            element.name = element.originalName + " (Last: " + dateStr + ")"
-        },
-        onClickCheck(checked){
-            let codes = _.map(checked, 'code');
-
-            this.$apollo.mutate({
-                mutation: CHECK_MUTATION,
-                variables : {
-                    done:{
-                        _id: this.node.data.checklist,
-                        codes: codes
-                    }
-                },
-            }).then(res => {
-                // Update date
-                checked.forEach(element => {
-                    if (res.data.check.codes.includes(element.code)){
-                        this.setElementDate(element, res.data.check.date)
-                    }
-                });
-            });
-        },
-        onClickUncheck(unchecked){
-            let codes = _.map(unchecked, 'code');
-
-            this.$apollo.mutate({
-                mutation: UNCHECK_MUTATION,
-                variables : {
-                    undo:{
-                        _id: this.node.data.checklist,
-                        codes: codes
-                    }
-                },
-            }).then(res => {
- 
-            });
-        },
-        onClickDeactivate(item){
-            this.$apollo.mutate({
-                mutation: DEACTIVATE_MUTATION,
-                variables : {
-                    deactivate:{
-                        _id: this.node.data.checklist,
-                        codes: [item.code]
-                    }
-                },
-            }).then(res => {
-                this.deactivated = _.union(this.deactivated, res.data.deactivate);
-                item.deactivated = true;
-
-                // force to rerender
-                this.$forceUpdate();
-            });
-        },
-        onClickActivate(item){
-            this.$apollo.mutate({
-                mutation: ACTIVATE_MUTATION,
-                variables : {
-                    activate:{
-                        _id: this.node.data.checklist,
-                        codes: [item.code]
-                    }
-                },
-            }).then(res => {
-                this.deactivated = _.difference(this.deactivated, res.data.activate);
-                item.deactivated = false;
-
-                // force to rerender
-                this.$forceUpdate();
-            });
         },
         onChangeNodetype(selectedType){
             // get filtered vultype
---
+++ front/src/components/infocard/checklist/ChecklistTree.vue
@@ -0,0 +1,283 @@
+<template>
+    <v-treeview
+        :key="Math.random()"
+        open-all
+        :items="renderTree"
+        item-key="code"
+        selectable
+        selected-color="#ff2a6d"
+        selection-type="leaf"
+        item-disabled="deactivated"
+        dense
+        shaped
+        hoverable
+        v-model="done"
+        return-object
+    >
+        <template #append="{item}">
+            <v-btn
+                v-if="deactivated.includes(item.code)"
+                icon
+                @click="onClickActivate(item)"
+            >
+                <v-icon>mdi-eye-check-outline</v-icon>
+            </v-btn>
+            <v-btn
+                v-else
+                icon
+                @click="onClickDeactivate(item)"
+            >
+                <v-icon>mdi-eye-off-outline</v-icon>
+            </v-btn>
+        </template>
+    </v-treeview>
+</template>
+<script>
+
+import _ from 'lodash'
+
+import moment from 'moment'
+import 'moment-timezone'
+
+import {
+    CHECKLIST_QUERY,
+    CHECK_MUTATION,
+    UNCHECK_MUTATION,
+    ACTIVATE_MUTATION,
+    DEACTIVATE_MUTATION,
+} from '@/graphql/checklist'
+
+export default {
+    name: "ChecklistTree",
+    props: {
+        node: {
+            type: Object
+        },
+        original: {
+            type: Array
+        }
+    },
+    watch: {
+        node: {
+            handler(next, prev){
+                this.initNode();
+            },
+        },
+        original: {
+            deep: true,
+            handler(next){
+                this.initTree(next, this.node);
+
+                // Init timestamp, deactivated
+                // Call directly becuase timestamp/deactivated values are not changed
+                this.updateTimestamp(this.timestamp);
+                this.updateDeactivated(this.deactivated);
+            }
+        },
+        timestamp: {
+            handler(next, prev){
+                // Should called after render
+                if(this.original.length === 0){
+                    return;
+                }
+
+                let updated = _.differenceBy(next, prev, 'date');
+                this.updateTimestamp(updated);
+            }
+        },
+        deactivated: function(next, prev){
+            // Should called after render
+            if(this.original.length === 0){
+                return;
+            }
+            let updated = _.differenceWith(next, prev, _.isEqual);
+            this.updateDeactivated(updated);
+        },
+        done: function(next, prev){
+            // Should work after first time
+            if(!this.doneFirstWatch){
+                this.doneFirstWatch = true;
+                return;
+            }
+
+            if (next.length > prev.length){
+                // If Check
+                // Get new checked list
+                let newCheck = _.differenceBy(next, prev, 'code');
+                this.onClickCheck(newCheck);
+            }
+            else{
+                // If Uncheck
+                let unCheck = _.differenceBy(prev, next, 'code');
+                this.onClickUncheck(unCheck);
+            }
+        },
+    },
+    data(){
+        return{
+            originalArray: [],
+
+            renderTree: [],
+            renderArray: [], // Each element of tree array
+
+            done: [],
+            timestamp: [],
+            deactivated: [],
+
+            doneFirstWatch: false,
+        }
+    },
+    methods: {
+        initNode(){
+            this.doneFirstWatch = false;
+        },
+        initTree(original, node){
+            if (original.length === 0){
+                return;
+            }
+
+            this.originalArray = [];
+            this.treeToArray(original, this.originalArray);
+
+            this.initRender(original);
+    
+            // Fetch node checklist, Add date info
+            this.fetchChecklist(node);
+        },
+        initRender(checklist){
+            if (!checklist){
+                return;
+            }
+
+            this.renderTree = JSON.parse(JSON.stringify(checklist));
+
+            this.renderArray = [];
+            this.treeToArray(this.renderTree, this.renderArray);
+        },
+        treeToArray(tree, result){
+            for(let key in tree){
+                result.push(tree[key])
+                if(tree[key].children){
+                    this.treeToArray(tree[key].children, result);
+                }
+            }
+        },
+        updateTimestamp(updated){
+            updated.forEach(element => {
+                let originalItem = _.find(this.originalArray, { code: element.code })
+                if (originalItem){ // If checklist doesn't inlcude code, skip this.
+                    let renderItem = _.find(this.renderArray, { code: element.code })
+
+                    const dateStr = moment(element.date).format('YYYY-MM-DD HH:mm:ss');
+                    renderItem.name = originalItem.name + " (Updated: " + dateStr + ")"
+                }
+            });
+        },
+        updateDeactivated(deactivated){
+            deactivated.forEach(element => {
+                let renderItem = _.find(this.renderArray, { code: element })
+                if(renderItem){ // If checklist doesn't inlcude code, skip this.
+                    renderItem.deactivated = true;
+                }
+            });
+        },
+        fetchChecklist(node){
+            // get done/deactivated list
+            this.$apollo.query({
+                query: CHECKLIST_QUERY,
+                variables : {
+                    id: node.data.checklist,
+                },
+            }).then(res => {
+                this.done = JSON.parse(res.data.checklist.done);
+                this.timestamp = JSON.parse(res.data.checklist.timestamp);
+                this.deactivated = res.data.checklist.deactivated;
+            });
+        },
+        onClickCheck(checked){
+            let codes = _.map(checked, 'code');
+
+            this.$apollo.mutate({
+                mutation: CHECK_MUTATION,
+                variables : {
+                    done:{
+                        _id: this.node.data.checklist,
+                        codes: codes
+                    }
+                },
+            }).then(res => {
+                // Update date
+                let newTimestamp = _.cloneDeep(this.timestamp);
+
+                checked.forEach(element => {
+                    if (res.data.check.codes.includes(element.code)){
+                        let old = _.find(newTimestamp, { code: element.code })
+
+                        if(old){
+                            old.date = res.data.check.date;
+                        }
+                        else{
+                            newTimestamp.push({
+                                code: element.code,
+                                date: res.data.check.date
+                            });
+                        }
+                    }
+                });
+
+                this.timestamp = newTimestamp;
+            });
+        },
+        onClickUncheck(unchecked){
+            let codes = _.map(unchecked, 'code');
+
+            this.$apollo.mutate({
+                mutation: UNCHECK_MUTATION,
+                variables : {
+                    undo:{
+                        _id: this.node.data.checklist,
+                        codes: codes
+                    }
+                },
+            }).then(res => {
+ 
+            });
+        },
+        onClickDeactivate(item){
+            this.$apollo.mutate({
+                mutation: DEACTIVATE_MUTATION,
+                variables : {
+                    deactivate:{
+                        _id: this.node.data.checklist,
+                        codes: [item.code]
+                    }
+                },
+            }).then(res => {
+                this.deactivated = _.union(this.deactivated, res.data.deactivate);
+
+                // force to rerender
+                this.$forceUpdate();
+            });
+        },
+        onClickActivate(item){
+            this.$apollo.mutate({
+                mutation: ACTIVATE_MUTATION,
+                variables : {
+                    activate:{
+                        _id: this.node.data.checklist,
+                        codes: [item.code]
+                    }
+                },
+            }).then(res => {
+                this.deactivated = _.difference(this.deactivated, res.data.activate);
+
+                // force to rerender
+                this.$forceUpdate();
+            });
+        },
+    },
+    created(){
+        this.initTree(this.original, this.node);
+    }
+}
+</script>
