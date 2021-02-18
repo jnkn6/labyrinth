b41918d71c86e5418ae175178e428755a3dc2e3a
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-02-19 08:49:30 +0900

    server: Refactoring routes - Merge /domain, /page, /component by using parameter at checklist

--- routes/checklist.js
+++ routes/checklist.js
@@ -1,86 +1,41 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 
-import checklist from '../resources/checklist/checklist'
+import { checklist, configure } from '../resources/checklist/checklist'
 
 const parser = bodyParser.json()
 
 const router = express.Router();
 
-router.post('/domain', parser, (req, res) => {
-    const name = req.body.name;
-    const expand = req.body.expand;
-
-    if (!name){
-        return res.status(404).send("Checklist not found.");
-    }
-
-    if (!checklist.domainChecklist[name]){
-        return res.status(404).send("Checklist not found.");
-    }
-
-    let result = {};
-
-    if (expand){
-        result = JSON.stringify(checklist.domainChecklist[name + "_expand"])
-    }
-    else{
-        result = JSON.stringify(checklist.domainChecklist[name])
-    }
-
-    return res.send(result)
-});
-
-router.post('/page', parser, (req, res) => {
-    const name = req.body.name;
-    const expand = req.body.expand;
-
-    if (!name){
-        return res.status(404).send("Checklist not found.");
-    }
-
-    if (!checklist.pageChecklist[name]){
-        return res.status(404).send("Checklist not found.");
-    }
-
-    let result = {};
-
-    if (expand){
-        result = JSON.stringify(checklist.pageChecklist[name + "_expand"])
-    }
-    else{
-        result = JSON.stringify(checklist.pageChecklist[name])
-    }
-
-    return res.send(result)
-});
-
-router.post('/component', parser, (req, res) => {
-    const name = req.body.name;
-    const expand = req.body.expand;
-
-    if (!name){
-        return res.status(404).send("Checklist not found.");
-    }
-
-    if (!checklist.componentChecklist[name]){
-        return res.status(404).send("Checklist not found.");
-    }
-
-    let result = {};
-
-    if (expand){
-        result = JSON.stringify(checklist.componentChecklist[name + "_expand"])
-    }
-    else{
-        result = JSON.stringify(checklist.componentChecklist[name])
-    }
-
-    return res.send(result)
-});
-
 router.get('/conf.json', (req, res) => {
-    return res.send(checklist.configure)
+    return res.send(configure)
 });
+
+router.post('/:key', parser, (req, res) => {
+    const key = req.params.key;
+
+    const name = req.body.name;
+    const expand = req.body.expand;
+
+    if (!key || !name){
+        return res.status(404).send("Checklist not found.");
+    }
+
+    if(!checklist[key] || !checklist[key][name]){
+        return res.status(404).send("Checklist not found.");
+    }
+
+    let result = {};
+
+    if (expand){
+        result = JSON.stringify(checklist[key][name + "_expand"])
+    }
+    else{
+        result = JSON.stringify(checklist[key][name])
+    }
+
+    return res.send(result)
+});
+
 
 export default router;
