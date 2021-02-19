48ae33ad4c0bb2c29236a3c11ef7f9b3a9023e9a
jnkn6 <72091653+jnkn6@users.noreply.github.com>
2021-02-20 07:24:54 +0900

    server: Update routes - Add /nodetype/vul route

--- routes/nodetype.js
+++ routes/nodetype.js
@@ -3,9 +3,34 @@ import bodyParser from 'body-parser'
 
 import nodetype from '../resources/nodetype'
 
-const parser = bodyParser.text({type: 'text/plain'})
+const parser = bodyParser.json()
 
 const router = express.Router();
+
+router.post('/vul', parser, (req, res) => {
+    const key = req.body.key; // ex) 'domain' or 'page' or 'component'
+    const types = req.body.nodetype; // ex) ['form', 'form_checkbox']
+
+    if (!key){
+        return res.status(404).send("nodetype not found.");
+    }
+
+    if (!nodetype.nodetypeOrder[key]){
+        return res.status(404).send("nodetype not found.");
+    }
+
+    let result = [];
+
+    types.forEach(type => {
+        if (nodetype.nodetype[key][type] && nodetype.nodetype[key][type].vul){
+            result = result.concat(nodetype.nodetype[key][type].vul);
+        }
+    });
+
+    result = [...new Set(result)];
+
+    return res.send(result)
+});
 
 router.post('/:key', parser, (req, res) => {
     const key = req.params.key;
